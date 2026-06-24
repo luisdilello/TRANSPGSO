2022a017a7118949df2f71c73f2759d6d1696f97
TransPgso <luis@transpgso.cl>
2026-06-24 04:09:16 +0000

    fix: quitar debug alert + SW v1782274151 fuerza recarga cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// TransPgso SW v1782273444 - FUERZA NO CACHE
-const APP_VERSION = '1782273444';
+// TransPgso SW v1782274151 - FUERZA NO CACHE
+const APP_VERSION = '1782274151';
 
 self.addEventListener('install', e => {
   console.log('SW v' + APP_VERSION + ' instalando');
