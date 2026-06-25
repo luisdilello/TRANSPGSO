6dba4632ae04e7201a389be4f14286f2b7fba284
TransPgso <luis@transpgso.cl>
2026-06-25 02:39:33 +0000

    fix: SW v1782355168 fuerza recarga cache en movil

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// TransPgso SW v1782273444 - FUERZA NO CACHE
-const APP_VERSION = '1782273444';
+// TransPgso SW v1782355168 - FUERZA NO CACHE
+const APP_VERSION = '1782355168';
 
 self.addEventListener('install', e => {
   console.log('SW v' + APP_VERSION + ' instalando');
