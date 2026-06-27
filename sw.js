d5997fdab96ec2b2c6580c871076c919a3ffa0cb
TransPgso <luis@transpgso.cl>
2026-06-27 04:55:23 +0000

    fix: base 42f8b5a + modelo haiku-4-5 + foto comprimida v1782536123

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// TransPgso SW v1782536011
-const APP_VERSION = '1782536011';
+// TransPgso SW v1782536123
+const APP_VERSION = '1782536123';
 self.addEventListener('install', e => { e.waitUntil(self.skipWaiting()); });
 self.addEventListener('activate', e => {
   e.waitUntil(Promise.all([caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))),self.clients.claim()]));
