e621541d1f94aa9757f88239a20a6d38f0bd2802
TransPgso <luis@transpgso.cl>
2026-06-27 04:13:08 +0000

    revert: volver a fcfb600 antes del boton rellenar datos v1782533588

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// TransPgso SW v1782533426
-const APP_VERSION = '1782533426';
+// TransPgso SW v1782533588
+const APP_VERSION = '1782533588';
 self.addEventListener('install', e => { e.waitUntil(self.skipWaiting()); });
 self.addEventListener('activate', e => {
   e.waitUntil(Promise.all([caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))),self.clients.claim()]));
