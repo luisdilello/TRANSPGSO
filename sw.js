5e7aade74b3fbae272d9c12c1fbd0007c48a5f2e
TransPgso <luis@transpgso.cl>
2026-06-27 16:11:21 +0000

    fix: perfil badge sin emoji/caracter suelto v1782576681

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// TransPgso SW v1782576551
-const APP_VERSION = '1782576551';
+// TransPgso SW v1782576681
+const APP_VERSION = '1782576681';
 self.addEventListener('install', e => { e.waitUntil(self.skipWaiting()); });
 self.addEventListener('activate', e => {
   e.waitUntil(Promise.all([caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))),self.clients.claim()]));
