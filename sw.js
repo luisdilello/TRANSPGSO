1b0f1964f02e4eacfa67c65b4299f91864460dd3
TransPgso <luis@transpgso.cl>
2026-06-27 05:48:43 +0000

    fix: dashboard elimina estados obsoletos sin_asignar/asignado/pendiente_retiro v1782539323

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// TransPgso SW v1782539056
-const APP_VERSION = '1782539056';
+// TransPgso SW v1782539323
+const APP_VERSION = '1782539323';
 self.addEventListener('install', e => { e.waitUntil(self.skipWaiting()); });
 self.addEventListener('activate', e => {
   e.waitUntil(Promise.all([caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))),self.clients.claim()]));
