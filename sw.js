d2d4121c1cfc7ccd9f543875d06503df05f3593f
TransPgso <luis@transpgso.cl>
2026-06-28 23:55:30 +0000

    fix: ColectaRider guarda foto_etiqueta en insert y update v1782690930

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// TransPgso SW v1782690538
-const APP_VERSION = '1782690538';
+// TransPgso SW v1782690930
+const APP_VERSION = '1782690930';
 self.addEventListener('install', e => { e.waitUntil(self.skipWaiting()); });
 self.addEventListener('activate', e => { e.waitUntil(Promise.all([caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))),self.clients.claim()])); });
 self.addEventListener('fetch', e => {
