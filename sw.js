571e10fceac96044b7b27b1be28de5b5a7a35034
TransPgso <luis@transpgso.cl>
2026-06-28 22:34:48 +0000

    fix: valores modal font-weight 500 sin negrita v1782686088

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// TransPgso SW v1782685963
-const APP_VERSION = '1782685963';
+// TransPgso SW v1782686088
+const APP_VERSION = '1782686088';
 self.addEventListener('install', e => { e.waitUntil(self.skipWaiting()); });
 self.addEventListener('activate', e => { e.waitUntil(Promise.all([caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))),self.clients.claim()])); });
 self.addEventListener('fetch', e => {
