078013a404a97140563988bbb31006a2684a18ab
TransPgso <luis@transpgso.cl>
2026-06-29 01:24:14 +0000

    fix: ImportarPDFColecta renderiza cada pagina como foto_etiqueta y la sube a Supabase v1782696254

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// TransPgso SW v1782695558
-const APP_VERSION = '1782695558';
+// TransPgso SW v1782696254
+const APP_VERSION = '1782696254';
 self.addEventListener('install', e => { e.waitUntil(self.skipWaiting()); });
 self.addEventListener('activate', e => { e.waitUntil(Promise.all([caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))),self.clients.claim()])); });
 self.addEventListener('fetch', e => {
