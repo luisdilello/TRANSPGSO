91260b5a9b7e179a27a1e4a9ddc628cd493e21fa
TransPgso <luis@transpgso.cl>
2026-06-29 00:39:04 +0000

    feat: header tabla Bebas 12px glow + comunas 12px legibles sin nowrap v1782693544

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// TransPgso SW v1782693255
-const APP_VERSION = '1782693255';
+// TransPgso SW v1782693544
+const APP_VERSION = '1782693544';
 self.addEventListener('install', e => { e.waitUntil(self.skipWaiting()); });
 self.addEventListener('activate', e => { e.waitUntil(Promise.all([caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))),self.clients.claim()])); });
 self.addEventListener('fetch', e => {
