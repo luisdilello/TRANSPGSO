b1cf672115fc077e1028c281598ec0e8d5158e4a
TransPgso <luis@transpgso.cl>
2026-06-29 15:12:13 +0000

    fix: App sincroniza mensajeros desde Supabase al iniciar y normaliza nombres v1782745933

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// TransPgso SW v1782745587
-const APP_VERSION = '1782745587';
+// TransPgso SW v1782745933
+const APP_VERSION = '1782745933';
 const CACHE_NAME = 'transpgso-' + APP_VERSION;
 self.addEventListener('install', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()).then(() => self.clients.matchAll({type:'window'})).then(clients => clients.forEach(c => c.postMessage({type:'SW_UPDATED',version:APP_VERSION})))); });
