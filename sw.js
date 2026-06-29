b77cefd5b448bbef35c410b2dc415afdfaec32f5
TransPgso <luis@transpgso.cl>
2026-06-29 01:12:38 +0000

    feat: dropdown cliente en tabla + cambio masivo de cliente con modal v1782695558

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// TransPgso SW v1782695346
-const APP_VERSION = '1782695346';
+// TransPgso SW v1782695558
+const APP_VERSION = '1782695558';
 self.addEventListener('install', e => { e.waitUntil(self.skipWaiting()); });
 self.addEventListener('activate', e => { e.waitUntil(Promise.all([caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))),self.clients.claim()])); });
 self.addEventListener('fetch', e => {
