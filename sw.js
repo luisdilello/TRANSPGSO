498e60d4616cfaaf1d8df0c1baa91a6bfad68387
TransPgso <luis@transpgso.cl>
2026-06-29 02:08:53 +0000

    fix: parser PDF ignora zonas ANILLO ESTE/NORTE/SUR antes de la comuna real v1782698933

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// TransPgso SW v1782698451
-const APP_VERSION = '1782698451';
+// TransPgso SW v1782698933
+const APP_VERSION = '1782698933';
 self.addEventListener('install', e => { e.waitUntil(self.skipWaiting()); });
 self.addEventListener('activate', e => { e.waitUntil(Promise.all([caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))),self.clients.claim()])); });
 self.addEventListener('fetch', e => {
