fa41067d16fc8e22aaed037fc1a53bd3b64d544b
TransPgso <luis@transpgso.cl>
2026-06-29 14:46:06 +0000

    debug: logs en update mensajero para diagnosticar fallo en Supabase v1782744366

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// TransPgso SW v1782744193
-const APP_VERSION = '1782744193';
+// TransPgso SW v1782744366
+const APP_VERSION = '1782744366';
 self.addEventListener('install', e => { e.waitUntil(self.skipWaiting()); });
 self.addEventListener('activate', e => { e.waitUntil(Promise.all([caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))),self.clients.claim()])); });
 self.addEventListener('fetch', e => {
